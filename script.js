1f381abf7da7b24940140a94d7bba5f472a08f05
Artsiom Kosmin <artyom17may@gmail.com>
2021-08-06 23:40:21 +0300

    Доп урок 4

--- script.js
+++ script.js
@@ -1,62 +1,21 @@
-let leng = prompt('Выберите язык ru/en');
+'use strict';
 
-if (leng === 'ru') {
-    console.log('Понедельник');
-    console.log('Вторник');
-    console.log('Среда');
-    console.log('Четверг');
-    console.log('Пятница');
-    console.log('Суббота');
-    console.log('Воскресенье');
-} else if (leng === 'en') {
-    console.log('Monday');
-    console.log('tuesday');
-    console.log('wednesday');
-    console.log('thursday');
-    console.log('friday');
-    console.log('saturday');
-    console.log('sunday');
-} else {
-    console.log('Введите en или ru');
-}
+let text = prompt('Введите текст', ' проверка на правильную работу12345678910111213141516171819 ');
 
 
-switch (leng) {
-    case 'ru':
-        console.log('Понедельник');
-        console.log('Вторник');
-        console.log('Среда');
-        console.log('Четверг');
-        console.log('Пятница');
-        console.log('Суббота');
-        console.log('Воскресенье');
-        break;
-    
-    case 'en':
-        console.log('Monday');
-        console.log('tuesday');
-        console.log('wednesday');
-        console.log('thursday');
-        console.log('friday');
-        console.log('saturday');
-        console.log('sunday');
-        break;
+let additionalTask = function(text) {
+    let check = parseFloat(text);
+    if (!isNaN(check)) {
+        alert('Введите текст!');
+    } else if (text.trim().length >= 30) {
+        let intermediateText = text.trim();
+        let replacement = intermediateText.slice(30);
+        let finalText = intermediateText.replace(replacement, '...');
+        return console.log(finalText);
+    } else {
+        let finalText = text.trim();
+        return console.log(finalText);
+    }
+};
 
-    default:
-        console.log('Введите en или ru');
-        break;
-}
-
-
-let mas = [];
-    mas['ru'] =  ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-    mas['en'] = ['Monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-
-console.log(mas[leng]);   
-
-
-let namePerson = prompt('Введите имя человека');
-let positionHeld = namePerson === 'Артем' ? "Директор" : namePerson === 'Максим' ? "Преподователь" : "Студент";
-console.log(positionHeld);
-
-
+console.log(additionalTask(text));
