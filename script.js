2d9d561a0bf0c1714d824bb4383edaba8116d6d6
Artsiom Kosmin <artyom17may@gmail.com>
2021-08-09 04:33:11 +0300

    Доп урок 7

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 'use strict';
 
-let arr = ['3245', '234435', '3245', '789678', '45463', '25443', '243269'];
-console.log(arr);
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+console.log(week);
+document.write(week);
+document.write('<br \/>');
 
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i].charAt(0) === '2' || arr[i].charAt(0) === '4') {
-        console.log(arr[i]);
-    }
+
+
+for (let i = 0; i < week.length; i++) { // Вопрос, почему writeln не записывает в новую строку?
+    console.log(week[i]);
+    document.writeln(week[i]);
+    document.write('<br \/>');
 }
 
 
-let check = 0;
-for (let number = 0; number < 101; number++) {
-    for (let i = 1; i < number + 1; i++) {
-        if (number % i === 0) {
-            check++;
-        }
+
+
+
+for (let i = 0; i < week.length; i++) {
+    if (i === 5 || i === 6) {
+        console.log(week[i].italics());
+        document.write(' ' + week[i].italics());
+    } else {
+        console.log(week[i]);
+        document.write(' ' + week[i]);
     }
-    if (check === 2) {
-        console.log('Делители этого числа 1 и', number);
+}
+
+document.write('<br \/>');
+
+let date = new Date();
+let date1 = date.getDay();
+console.log(date1);
+
+for (let i = 1; i < week.length + 1; i++) {
+    if (i === date1) {
+        console.log(week[i - 1].bold());
+        document.write(' ' + week[i - 1].bold());
+    } else {
+        console.log(week[i - 1]);
+        document.write(' ' + week[i - 1]);
     }
-    check = 0;
 }
